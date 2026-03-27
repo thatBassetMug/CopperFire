5939b31c7b28ff85763f6cf1c3c3de314fc9e633
Amanda Basset <j.amanda.basset@gmail.com>
2026-03-27 14:41:10 -0500

    Initial Commit

---
+++ CopperFire/CopperFireApp.swift
@@ -0,0 +1,32 @@
+//
+//  CopperFireApp.swift
+//  CopperFire
+//
+//  Created by Amanda Basset on 3/27/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CopperFireApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
